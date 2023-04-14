ea79ef56ad53db6a3e7efd048c7d10eaeebfed15
JABAN111 <130747945+JABAN111@users.noreply.github.com>
2023-04-14 12:43:52 +0300

    Add files via upload

---
+++ testClass.java
@@ -0,0 +1,10 @@
+package lab5;
+
+public class testClass {
+    private static String jo;
+    testClass(String input){jo = input;}
+
+    public static String getJo() {
+        return jo;
+    }
+}
